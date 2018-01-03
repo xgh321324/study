87fbb24db06c34a1ccda68c54af4d252e0bcad37
xuguanghui <970185127@qq.com>
2018-01-03 21:19:46 +0800

    悠悠课堂 学习selenium
    18年1月3

---
+++ father_xpath.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+from selenium import webdriver
+import time
+
+browser = webdriver.Chrome()
+browser.maximize_window()
+browser.get('http://www.baidu.com')
+browser.find_element_by_xpath('//form[@id="form"]/span/input').send_keys('屈小杰')  #其实这是利用爷爷级别的属性来定位
+browser.find_element_by_id('su').click()
+print('测试结束')
+browser.close()
+browser.quit()
+#kandao 64y页（未）
